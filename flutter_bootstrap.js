02d1b422defa707d96067bacc4cb2aee8d4d9de9
grove86dipesh <85902377+grove86dipesh@users.noreply.github.com>
2025-04-18 12:47:57 +0530

    Deploy Version 1.0.2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3218148198"
+    serviceWorkerVersion: "428233161"
   }
 });
